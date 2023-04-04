65d5d27e88052a1d765848023dc28127eaf808e5
Michael chinye <michaelchinye2018@gmail.com>
2023-04-04 15:09:20 +0100

    added redirect link for cpanel hosting sake

--- src/components/Login.js
+++ src/components/Login.js
@@ -33,6 +33,7 @@ function Login() {
                          setAlertTitle('Success!');
                          setIsLoading(false);
                          setShowAlert(true);
+                         window.location.href='https://loveworldteensandyouthministry.org/itplc'
                     }else{
                          setAlertText(data.message);
                          setAlertTitle('Error!');
